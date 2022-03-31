ae0141cdf0c3de4bf52dd98d135e27947e6233bb
palbrcht <palbrcht@gmail.com>
2022-03-30 21:47:58 -0500

    added generic drug and geography queries

--- querying_functions.py
+++ querying_functions.py
@@ -21,10 +21,77 @@ def fetch_provider(provider_last_name, provider_first_name = None):
     return(placeholder_df)
 
 ###
+###  Example use:  fetch_provider('Smith' , 'John')
+###
 ###  Last name is required.
 ###  First name is optional.
 ###
-###  Example use:
+
+def fetch_state(state_abbreviation):
+
+    cms_domain = 'https://data.cms.gov/data-api/v1/dataset/5a27f7a8-c7af-434f-a26c-54db03e22cd1/data?'
+    api_filter_last_name = ('&filter[Prscrbr_State_Abrvtn]=' + state_abbreviation)
+    query_string = (cms_domain + api_filter_last_name )
+
+    api_request = requests.get(query_string)
+    json_data = api_request.json()
+    placeholder_df = pandas.DataFrame.from_dict(json_data)
+    print(placeholder_df)
+    return(placeholder_df)
+
+### Example:  fetch_state('TN')
+
+def fetch_city(city_name, state_abbreviation):
+
+    cms_domain = 'https://data.cms.gov/data-api/v1/dataset/5a27f7a8-c7af-434f-a26c-54db03e22cd1/data?'
+    api_filter_last_name = ('&filter[Prscrbr_State_Abrvtn]=' + state_abbreviation + '&filter[Prscrbr_City]=' + city_name)
+    query_string = (cms_domain + api_filter_last_name )
+
+    api_request = requests.get(query_string)
+    json_data = api_request.json()
+    placeholder_df = pandas.DataFrame.from_dict(json_data)
+    print(placeholder_df)
+    return(placeholder_df)
+
+### Example: fetch_city('Memphis' , 'TN')
+
+
+###
+###  TODO
+###
+### 1. Add cost filtering component
+### 2. Probably need a way to tidy up the if logic
 ###
 
-fetch_provider('INSERT_LAST_NAME' , 'INSERT_FIRST_NAME')
+def fetch_generic_drug(generic_drug_name, claims_count = None , claims_indication = None , total_cost = None):
+
+    cms_domain = 'https://data.cms.gov/data-api/v1/dataset/5a27f7a8-c7af-434f-a26c-54db03e22cd1/data?'
+
+    if claims_count == None and total_cost == None:
+        api_filter = ('&filter[Gnrc_Name]=' + generic_drug_name)
+        query_string = (cms_domain + api_filter)
+
+    if claims_count != None and total_cost == None:
+
+        if claims_indication == 'greater':
+            api_filter = ('&filter[Gnrc_Name]=' + generic_drug_name + '&filter[name-filter][condition][path]=Tot_Clms&filter[name-filter][condition][operator]=>&filter[name-filter][condition][value][1]=' + str(claims_count))
+            query_string = (cms_domain + api_filter)
+            print(query_string)
+
+        if claims_indication == 'lesser':
+            api_filter = ('&filter[Gnrc_Name]=' + generic_drug_name + '&filter[name-filter][condition][path]=Tot_Clms&filter[name-filter][condition][operator]=<&filter[name-filter][condition][value][1]=' + str(claims_count))
+            query_string = (cms_domain + api_filter)
+
+    api_request = requests.get(query_string)
+    json_data = api_request.json()
+    placeholder_df = pandas.DataFrame.from_dict(json_data)
+    print(placeholder_df)
+    placeholder_df.to_csv('part_d_data_export.csv', header=True, index=False)
+    return(placeholder_df)
+
+### Examples: 
+### 
+### fetch_generic_drug('Oxycodone Hcl')
+### 
+### fetch_generic_drug('Oxycodone Hcl')
+### fetch_generic_drug(generic_drug_name = 'Atorvastatin Calcium' , claims_count = 1500 , claims_indication = 'greater')
